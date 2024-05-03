a8ac4c1ae7f7bd925a5bf7004c00405d3629a42a
RitikGuleria05 <ritikguleria780@gmail.com>
2024-05-03 17:22:16 +0530

    Components Management

--- Mega-Blog/src/componenets/Header/Header.jsx
+++ Mega-Blog/src/componenets/Header/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import {Container, Logo, LogoutBtn} from '../index';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
 
 function Header() {
+    const authStatus = useSelector((state) => state.auth.authStatus)
+    const navigate = useNavigate()
+
+    const naItems = [
+        {
+
+        },{}
+    ]
     return (
-        <div>
-            Header
-        </div>
+        <div>Header</div>
     )
 }
 
