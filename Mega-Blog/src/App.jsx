b1340aa48f62b8e789fd5b77aaa3786d90ce67db
RitikGuleria05 <ritikguleria780@gmail.com>
2024-05-03 14:54:18 +0530

    Completed redux configuration

--- Mega-Blog/src/App.jsx
+++ Mega-Blog/src/App.jsx
@@ -2,20 +2,38 @@ import { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import './App.css'
 import authService from './appwrite/auth'
+import { login, logout } from "./store/authSlice"
+import { Footer, Header } from './componenets'
+import { Outlet } from 'react-router-dom'
 
 function App() {
   const [loading, setloading] = useState(true)
   const dispatch = useDispatch()
 
-  useEffect (() =>{
-    authService.getCurrentUser().then(() => {}).finally()
-  },[])
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        }
+        else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setloading(false))
+  }, [])
 
-  return (
-    <>
-      <h1>My Sanskari Blog</h1>
-    </>
-  )
+  return !loading ? (
+    <div className=' min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className=' w-full block'>
+        <Header />
+        <main>
+          TODO: <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
