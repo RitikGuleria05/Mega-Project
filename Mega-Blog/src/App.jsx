b3fa85c98677778702681b4d07bd47342b883bd0
RitikGuleria05 <ritikguleria780@gmail.com>
2024-05-20 17:54:09 +0530

    Debugging

--- Mega-Blog/src/App.jsx
+++ Mega-Blog/src/App.jsx
@@ -1,13 +1,13 @@
-import { useEffect, useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import './App.css'
-import authService from './appwrite/auth'
-import { login, logout } from "./store/authSlice"
+import authService from "./appwrite/auth"
+import {login, logout} from "./store/authSlice"
 import { Footer, Header } from './componenets'
 import { Outlet } from 'react-router-dom'
 
 function App() {
-  const [loading, setloading] = useState(true)
+  const [loading, setLoading] = useState(true)
   const dispatch = useDispatch()
 
   useEffect(() => {
@@ -19,15 +19,15 @@ function App() {
         dispatch(logout())
       }
     })
-    .finally(() => setloading(false))
+    .finally(() => setLoading(false))
   }, [])
-
+  
   return !loading ? (
-    <div className=' min-h-screen flex flex-wrap content-between bg-gray-400'>
-      <div className=' w-full block'>
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
         <Header />
         <main>
-          TODO: <Outlet />
+        TODO:  <Outlet />
         </main>
         <Footer />
       </div>
--- Mega-Blog/src/main.jsx
+++ Mega-Blog/src/main.jsx
@@ -2,16 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Provider } from 'react-redux';
-import store from './store/store.js';
+import { Provider } from 'react-redux'
+import store from './store/store.js'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Home from './Pages/Home.jsx'
 import { AuthLayout, Login } from './componenets/index.js'
-import AddPost from "./Pages/AddPost.jsx";
-import Signup from './Pages/Signup.jsx'
-import EditPost from "./Pages/EditPost.jsx";
-import Post from "./Pages/Post.jsx";
-import AllPosts from "./Pages/AllPosts.jsx";
+
+
+import AddPost from './Pages/AddPost.jsx'
+import Signup from './Pages/Signup'
+import EditPost from './Pages/EditPost.jsx'
+
+import Post from './Pages/Post.jsx'
+
+import AllPosts from './Pages/AllPosts.jsx'
 
 const router = createBrowserRouter([
     {
@@ -77,7 +81,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Provider store={store}>
             <RouterProvider router={router} />
-            <App />
         </Provider>
     </React.StrictMode>,
 )
