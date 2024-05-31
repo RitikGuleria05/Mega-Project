d5b5e52883ea2f4298ed81c36932ac0c6b3fd1bf
RitikGuleria05 <ritikguleria780@gmail.com>
2024-05-31 16:49:57 +0530

    Debuging

--- Mega-Blog/src/componenets/post-form/PostForm.jsx
+++ Mega-Blog/src/componenets/post-form/PostForm.jsx
@@ -113,7 +113,7 @@ export default function PostForm({ post }) {
                     className="mb-4"
                     {...register("status", { required: true })}
                 />
-                <Button type="submit" bgColor={post ? "bg-green-500" : undefined} className="w-full bg-blue-900">
+                <Button type="submit" bgColor={post ? "bg-green-500" : undefined} className="w-full bg-blue-700">
                     {post ? "Update" : "Submit"}
                 </Button>
             </div>
